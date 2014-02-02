68e71e625930f3663c77cd8a9ff429d51bc8e0ee
stuart.rose <stuartjrose99@gmail.com>
2014-02-02 15:08:09 +0100

    Initial commit

---
+++ combat.rb
@@ -0,0 +1,14 @@
+class Combat
+
+#attr_accesor :
+
+
+  def initialize (AttackingForce, DefendingForce, CombatLocation, CombatTerrain)
+    @AttackingForce = AttackingForce
+    @DefendingForce = DefendingForce
+    @CombatLocation = CombatLocation
+    @CombatTerrain = CombatTerrain
+
+
+  end
+end
